ab3c83f61c43baf39986dd431398145efd013173
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2026-05-12 15:50:22 +0600

    folder structure and GET , POST method

---
+++ restApiAndServerArchitechture/src/controllers/product.controller.ts
@@ -0,0 +1,51 @@
+import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../services/product.service";
+import type { IProduct } from "../types/product.type";
+import { parseBody } from "../utility/parseBody";
+
+export const productController = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
+  const url = req.url;
+  const method = req.method;
+
+  const urlParts = url?.split("/");
+
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
+
+  if (url === "/products" && method === "GET") {
+    const result = readProduct();
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Products Retrieve Successfully!",
+        data: result,
+      }),
+    );
+  } else if (method === "GET" && id !== null) {
+    const products = readProduct();
+    const product = products.find((p: IProduct) => p.id === id);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product Retrieve Successfully!",
+        data: product,
+      }),
+    );
+  } else if (method === "POST" && url === "/products") {
+    const body = await parseBody(req);
+
+    console.log(body);
+    const products = readProduct();
+    const product = products.find((p: IProduct) => p.id === id);
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Product Retrieve Successfully!",
+        data: product,
+      }),
+    );
+  }
+};
--- restApiAndServerArchitechture/src/server.ts
+++ restApiAndServerArchitechture/src/server.ts
@@ -1,24 +1,9 @@
 import { createServer, IncomingMessage, Server, ServerResponse } from "http";
+import { routeHandler } from "./routes/route";
 
 const server: Server = createServer(
   (req: IncomingMessage, res: ServerResponse) => {
-    console.log(req.url);
-    console.log(req.method);
-
-    const url = req.url;
-    const method = req.method;
-
-    if (url === "/" && method === "GET") {
-      console.log("this is root route");
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(JSON.stringify({ message: "Root route" }));
-    } else if (url?.startsWith("/products")) {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(JSON.stringify({ message: "This is product route" }));
-    } else {
-      res.writeHead(404, { "content-type": "application/json" });
-      res.end(JSON.stringify({ message: "Route not found!" }));
-    }
+    routeHandler(req, res);
   },
 );
 
